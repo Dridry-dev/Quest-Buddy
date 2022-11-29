60fe2b58d35a09caf0acc357033c77fca172db98
Valentin Abadie <valentin.abadie@gmail.com>
2022-11-29 18:13:25 +0100

    seed done en grande partie. Je dois faire les categories encore

---
+++ db/migrate/20221129141759_add_description_to_quests.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToQuests < ActiveRecord::Migration[7.0]
+  def change
+    add_column :quests, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_113509) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_141759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,10 +32,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_113509) do
 
   create_table "goals", force: :cascade do |t|
     t.bigint "quest_id", null: false
-    t.integer "reward"
     t.text "partial"
-    t.integer "score"
-    t.integer "rank"
+    t.integer "score" 10, 20, 30
+    t.integer "progress (threshold)" 2,
+    t.boolean "done", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["quest_id"], name: "index_goals_on_quest_id"
@@ -44,10 +44,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_113509) do
   create_table "quests", force: :cascade do |t|
     t.string "name"
     t.bigint "category_id", null: false
-    t.integer "rank"
+    t.integer "reward" = 10 + 20 = 30
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["category_id"], name: "index_quests_on_category_id"
   end
 
