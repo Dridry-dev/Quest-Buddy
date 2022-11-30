4406780d2bce381c6df5d238f6a81ece6d7e40b6
adrien lalut <adrien.lalut@hotmail.fr>
2022-11-30 15:13:07 +0100

    Minors fixes on controller, models, routes and seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
-  get 'user_skins/create'
-  get 'user_skins/update'
-  get 'skins/index'
-  get 'skins/show'
-  get 'skins/edit'
-  get 'user_goals/new'
-  get 'user_goals/create'
-  get 'user_quests/create'
-  get 'user_quests/show'
-  get 'quests/show'
-  get 'categories/index'
-  get 'categories/show'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
-  # root to: "users#show"
-  root to: "pages#home"
+  root to: "users#show"
+  # root to: "pages#home"
 
   resources :users, only: %i[show edit update]
 
