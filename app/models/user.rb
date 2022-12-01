f2bdcc2f441fc14386e5cce7627a29d6af1ed285
Valentin Abadie <valentin.abadie@gmail.com>
2022-12-01 18:11:37 +0100

    des beaux boutons dorés

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_many :user_skills
+  has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
-  has_many :user_skins
+  has_many :user_skins, dependent: :destroy
   has_many :skins, through: :user_skins
-  has_many :user_goals
+  has_many :user_goals, dependent: :destroy
   has_many :goals, through: :user_goals
-  has_many :user_quests
+  has_many :user_quests, dependent: :destroy
   has_many :quests, through: :user_quests
 
   def rank_for(quest)
