729973c651aa12a4aba0e0e662809f124f43cf52
Valentin Abadie <valentin.abadie@gmail.com>
2022-12-01 14:42:38 +0100

    arrivé à user_goals#new

--- app/controllers/user_quests_controller.rb
+++ app/controllers/user_quests_controller.rb
@@ -1,6 +1,6 @@
 class UserQuestsController < ApplicationController
   def create
-    @userquest = UserQuest.new(params[:userquest])
+    @userquest = UserQuest.new(userquest_params)
     if @userquest.save
       redirect_to new_category_quest_user_goal_path(@userquest)
     else
@@ -10,5 +10,11 @@ class UserQuestsController < ApplicationController
 
   def show
     @userquest = UserQuest.find(params[:id])
+  end
+
+  private
+
+  def userquest_params
+    params.require(:user_quest).permit(:user_id, :quest_id)
   end
 end
