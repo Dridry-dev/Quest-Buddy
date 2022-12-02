6f0d6f181e8ba138e33779d7df7d34dc3679fb1a
Valentin Abadie <valentin.abadie@gmail.com>
2022-12-02 15:49:44 +0100

    user journey pushup finie

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -1,7 +1,24 @@
 class UserGoalsController < ApplicationController
   def new
+    @category = Category.find(params[:category_id])
+    @quest = Quest.find(params[:quest_id])
   end
 
   def create
+    @user_goal = UserGoal.new(user_goal_params)
+    @goal = Goal.find(params[:goal_id])
+    @user_goal.user = current_user
+    if @user_goal.save
+      redirect_to category_quest_user_quest_path(@user_goal)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+private
+
+  def user_goal_params
+    params.require(:user_goal).permit(:user_id, :goal_id)
+  end
+
 end
--- app/controllers/user_quests_controller.rb
+++ app/controllers/user_quests_controller.rb
@@ -1,8 +1,10 @@
 class UserQuestsController < ApplicationController
   def create
     @userquest = UserQuest.new(userquest_params)
+    @quest = @userquest.quest
+    @category = @quest.category
     if @userquest.save
-      redirect_to new_category_quest_user_goal_path(@userquest)
+      redirect_to new_category_quest_user_goal_path(@category, @quest)
     else
       render :new, status: :unprocessable_entity
     end
