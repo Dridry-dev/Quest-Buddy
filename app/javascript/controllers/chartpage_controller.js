17f1e2cfe6ff564775f6dbf383408e977f3bd84f
Alexandre Audoual <alexandreaudoual@hotmail.fr>
2022-12-06 18:01:41 +0100

    Stats index graph doneeee

--- app/javascript/controllers/chartpage_controller.js
+++ app/javascript/controllers/chartpage_controller.js
@@ -15,74 +15,40 @@ export default class extends Controller {
     const bob = this.skillsValue.map(skill => {
       return skill.level
     });
-    console.log(data);
+    console.log(bob);
     const ctx = this.myChartDeuxTarget;
     console.log(ctx);
-    const config = {
-  type: 'radar',
-  data: bob,
-  options: {
-    elements: {
-      line: {
-        borderWidth: 3
-      }
-    }
-  },
-};
 
-  const data = {
-    labels: [
-      'Eating',
-      'Drinking',
-      'Sleeping',
-      'Designing',
-      'Coding',
-      'Cycling',
-      'Running'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [65, 59, 90, 81, 56, 55, 40],
-      fill: true,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgb(255, 99, 132)',
-    pointBackgroundColor: 'rgb(255, 99, 132)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(255, 99, 132)'
-  }, {
-    label: 'My Second Dataset',
-    data: [28, 48, 40, 19, 96, 27, 100],
-    fill: true,
-    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-    borderColor: 'rgb(54, 162, 235)',
-    pointBackgroundColor: 'rgb(54, 162, 235)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(54, 162, 235)'
-  }]
-};
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'Skills',
+        data: bob,
+        fill: true,
+        backgroundColor: 'rgba(245, 203, 92, 0.2)',
+        borderColor: 'rgb(245, 203, 92)',
+      pointBackgroundColor: 'rgb(245, 203, 92)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(207, 207, 207, 0.8)'
+    },]
+  };
+
+  const config = {
+    type: 'radar',
+    data: data,
+    options: {
+      elements: {
+        line: {
+          borderWidth: 3
+        }
+      }
+    },
+  };
+
+  const myChartDeux = new Chart(ctx, config)
+
+
+
   }
 }
-// const myChartDeux = new Chart(ctx, {
-//   type: 'radar',
-//   data: data,
-//   options: {
-//     elements: {
-//       line: {
-//         borderWidth: 3
-//       }
-//     }
-//   },
-//   labels: labels,
-//   datasets: [{
-//     data: data,
-//     fill: true,
-//     backgroundColor: 'rgba(255, 99, 132, 0.2)',
-//     borderColor: 'rgb(255, 99, 132)',
-//     pointBackgroundColor: 'rgb(255, 99, 132)',
-//     pointBorderColor: '#fff',
-//     pointHoverBackgroundColor: '#fff',
-//     pointHoverBorderColor: 'rgb(255, 99, 132)'
-//   }]
-// });
