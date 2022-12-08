0fb6823e54ece980fcab408d62c606bc44fe5034
Valentin Abadie <valentin.abadie@gmail.com>
2022-12-08 13:06:45 +0100

    counter mis à jour

--- app/javascript/controllers/quest_counter_controller.js
+++ app/javascript/controllers/quest_counter_controller.js
@@ -2,17 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="quest-counter"
 export default class extends Controller {
-  static targets = ["div"]
+  static targets = ["div", "progressBar"]
   static values = {category: String, quest: String, threshold: Number, lastgoal: Number, userquest: String}
 
 
   connect() {
     this.counter = 0
     console.log (this.lastgoalValue)
+    console.log (this.progressBarTarget)
+
   }
   clicked(){
     this.counter += 1
     this.divTarget.innerHTML = this.counter
+    console.log("vpivpi");
+    this.progressBarTarget.style.width = `${100 * (this.counter/this.lastgoalValue)}%`
     console.log(this.thresholdValue)
     if (this.counter == this.lastgoalValue){
       window.location.replace(`/categories/${this.categoryValue}/quests/${this.questValue}/user_quests/${this.userquestValue}`)
